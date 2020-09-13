6d08b43ad3307f990dd600ca128ae37afaf18c6d
Дмитрий <selyxaa@yandex.ru>
2020-09-13 20:56:02 +0300

    добавил оптимизацию картинок

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -18,6 +20,17 @@ module.exports = merge(common, {
                 }],
             },
             canPrint: true
+        }),
+        new ImageminPlugin({
+          optipng: {
+            optimizationLevel: 3
+          },
+          plugins: [
+            imageminMozjpeg({
+              quality: 90,
+              progressive: true
+            })
+          ]
         })
     ]
 });
